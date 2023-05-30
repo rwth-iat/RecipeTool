1dbfb586128be0d426ed7a59a38294b053bd031b
Sebastian Ulrich <sp.ulrich@hotmail.de>
2023-05-30 15:30:11 +0200

    Specified Doctype at the Begining This is recommended by the html specifications. Otherwise browsers will open it in quirks mode. To work properly some changes were made.

--- index.js
+++ index.js
@@ -43,51 +43,52 @@ function drop_handler(ev) {
 
     // If element is taken from the side_bar and dropped into the workspace clone the element
     // that way it still is in the side bar to be used multiple times
-    if (element.classList.contains("drag_copy") && ev.target.id == "workspace") {
-        console.log("dragged from sidebar, dropped in workspace");
-        //clone the element and add new clone to drop-div
+    if (element.classList.contains("side_bar_element") && ev.target.id == "workspace") {
+        //clone the element
         //generate a new unique id for the copied element
         var nodeCopy = element.cloneNode(true);
         nodeCopy.id = Date.now().toString(36) + Math.random().toString(36).substring(2);;
-        ev.target.appendChild(nodeCopy);
         
         // this is the actual "drag code"
-        nodeCopy.style.position = "absolute";
-        nodeCopy.style.width = "auto";
-        nodeCopy.style.height = "auto";
-        nodeCopy.style.left = ev.clientX;
-        nodeCopy.style.top = ev.clientY;
+        nodeCopy.style.left = ev.clientX + "px";
+        nodeCopy.style.top = ev.clientY + "px";
         nodeCopy.style.background = "white";
 
         //change classes to signify that the next time it is dragged it should be moved not copied
-        nodeCopy.classList.remove("drag_copy");
-        nodeCopy.classList.add("drag_move")
+        //also style is changed, for example is position set to "absolute"
+        nodeCopy.classList.remove("side_bar_element");
+        nodeCopy.classList.add("workspace_element");
 
         //reset border of the element
         nodeCopy.style.border = "1px solid black";
+        
+        //add new clone to document
+        ev.target.appendChild(nodeCopy);
+
+        console.log("dragged from sidebar, dropped in workspace at absolute position: " + ev.clientX.toString() + " " + ev.clientY.toString() );
     }
 
     // if element is in workspace and gets moved to workspace just move it
     // also check if target is itself so it can be sligthly moved
-     else if (element.classList.contains("drag_move") && (ev.target.id == "workspace" || ev.target.id==id)) {
-      console.log("dragged from workspace, dropped in workspace");
+     else if (element.classList.contains("workspace_element") && (ev.target.id == "workspace" || ev.target.id==id)) {
       // this is the actual "drag code"
-      element.style.position = "absolute";
-      element.style.width = "auto";
-      element.style.height = "auto";
-      element.style.left = ev.clientX;
-      element.style.top = ev.clientY;
+      element.style.left = ev.clientX +"px";
+      element.style.top = ev.clientY +"px";
       
+      console.log("dragged from workspace, dropped in workspace at absolute position: " + ev.clientX.toString() + " " + ev.clientY.toString() );
+
       //reset border of the element
       element.style.border = "1px solid black";
     }
 
     //if element is in workspace and gets moved to side_bar it gets removed
-    else if (element.classList.contains("drag_move") && getParents(ev.target).some(parent => parent.id == "side_bar")) {
+    else if (element.classList.contains("workspace_element") && getParents(ev.target).some(parent => parent.id == "side_bar")) {
       console.log("dragged from Workspace, dropped outside Workspace");
       element.remove();
     }
+    
 
+    console.log("dropped at " + ev.target.id.toString());
     // reset the border of the workspace
     //ev.target.style.border = "1px solid black";
 }
