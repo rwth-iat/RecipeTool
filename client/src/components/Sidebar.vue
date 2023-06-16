840035f82448b5bd11fae868986db2e64e6865fe
Sebastian Ulrich <sp.ulrich@hotmail.de>
2023-06-16 16:57:02 +0200

    fixed min height for sidebars

--- client/src/components/Sidebar.vue
+++ client/src/components/Sidebar.vue
@@ -107,7 +107,7 @@ aside {
 	color: var(--light);
 	width: calc(2rem + 32px);
 	overflow-y: scroll;
-	min-height: 100vh - var(--topbar-height);
+	min-height: calc(100vh - var(--topbar-height));
 	padding: 1rem;
 	transition: 0.2s ease-in-out;
 	.logo {
