dcf55b4c814ff7cb822d6b3303a7ffe30d7dbe0f
Sebastian Ulrich <sp.ulrich@hotmail.de>
2023-08-17 13:37:59 +0200

    moved export function into own file for overview

---
+++ client/src/components/export_xml.js
@@ -0,0 +1,181 @@
+//create an BatchML XML Document containing all Connections and Elements
+export function export_batchml(workspace_items, jsplumb_connections) {
+    // Create an XML document
+    var xmlDocument = document.implementation.createDocument("", "BatchInformation");
+    
+    // Specify the XML version at the top
+    const xmlDeclaration = xmlDocument.createProcessingInstruction('xml', 'version="1.0"');
+    xmlDocument.insertBefore(xmlDeclaration, xmlDocument.firstChild);
+    
+    // Get the root element
+    var batchML = xmlDocument.documentElement;
+    var general_recipe = xmlDocument.createElement('GeneralRecipe')
+    
+    //append top level elements
+    general_recipe.appendChild(xmlDocument.createElement('Description'));
+    general_recipe.appendChild(xmlDocument.createElement('ID'));
+    general_recipe.appendChild(xmlDocument.createElement('Version'));
+    
+    //create header and append
+    var header = xmlDocument.createElement('Header')
+    //modification log
+    var mod_log = xmlDocument.createElement('ModificationLog')
+    mod_log.appendChild(xmlDocument.createElement("ModifiedDate"));
+    mod_log.appendChild(xmlDocument.createElement("Author"));
+    header.appendChild(mod_log)
+    //aproval History
+    var approval_history = xmlDocument.createElement('ApprovalHistory')
+    approval_history.appendChild(xmlDocument.createElement('FinalApprovalDate'));
+    approval_history.appendChild(xmlDocument.createElement('Version'));
+    //individual approval
+    var individual_approval = xmlDocument.createElement('IndividualApproval');
+    individual_approval.appendChild(xmlDocument.createElement('ApprovedBy'));
+    individual_approval.appendChild(xmlDocument.createElement('ApprovalDate'));
+    approval_history.appendChild(individual_approval)
+    header.appendChild(approval_history)
+  
+    header.appendChild(xmlDocument.createElement('EffectiveDate'));
+    header.appendChild(xmlDocument.createElement('ProductID'));
+    header.appendChild(xmlDocument.createElement('ProductName'));
+    
+    // batch size
+    var batch_size = xmlDocument.createElement('BatchSize');
+    batch_size.appendChild(xmlDocument.createElement('Nominal'));
+    batch_size.appendChild(xmlDocument.createElement('Normal'));
+    batch_size.appendChild(xmlDocument.createElement('Min'));
+    batch_size.appendChild(xmlDocument.createElement('Max'));
+    batch_size.appendChild(xmlDocument.createElement('ScaleReference'));
+    batch_size.appendChild(xmlDocument.createElement('ScaledSize'));
+    batch_size.appendChild(xmlDocument.createElement('UnitOfMeasure'));
+    header.appendChild(batch_size)
+  
+    general_recipe.appendChild(header);
+  
+    //Prozess Procedure
+    var process_procedure = xmlDocument.createElement('ProcessProcedure')
+      // Iterate over workspace items and create XML elements
+      workspace_items.forEach(function (item) {
+        if(item.type == "process"){
+          var process_element = xmlDocument.createElement('ProcessElement');
+          process_element.setAttribute('id', item.id);
+          process_element.setAttribute('name', item.name);
+          process_element.setAttribute('type', item.type);
+          // You can add more attributes or data to the itemElement as needed
+          process_procedure.appendChild(process_element);
+        }
+      });
+    general_recipe.append(process_procedure)
+  
+    // Assuming you have a list of elements and connections
+    var connections = jsplumb_connections;
+  
+    var elementsWithSourceConnection = [];
+    var elementsWithTargetConnection = [];
+  
+    // Iterate through connections and collect elements
+    for (var connectionId in connections) {
+      var connection = connections[connectionId];
+      var sourceId = connection.sourceId;
+      var targetId = connection.targetId;
+  
+      // Find source and target elements
+      var sourceElement = document.getElementById(sourceId);
+      var targetElement = document.getElementById(targetId);
+  
+      // Check if source element already added to list
+      if (elementsWithSourceConnection.indexOf(sourceElement) === -1) {
+        elementsWithSourceConnection.push(sourceElement);
+      }
+  
+      // Check if target element already added to list
+      if (elementsWithTargetConnection.indexOf(targetElement) === -1) {
+        elementsWithTargetConnection.push(targetElement);
+      }
+    }
+  
+    console.log("Elements with source connection:", elementsWithSourceConnection);
+    console.log("Elements with target connection:", elementsWithTargetConnection);
+  
+    // Adds either input or output materials to xml file
+    function add_materials_to_xml(root_element, workspace_items, elements_list, material_type){
+      var materials = xmlDocument.createElement(material_type)
+      elements_list.forEach(function (item) {
+        var workspace_item = workspace_items.find(x => x.id === item.id);
+        if(workspace_item.type == "material"){
+          var process_element = xmlDocument.createElement('ProcessElement');
+          process_element.setAttribute('id', workspace_item.id);
+          process_element.setAttribute('name', workspace_item.name);
+          process_element.setAttribute('type', workspace_item.type);
+          // You can add more attributes or data to the itemElement as needed
+          materials.appendChild(process_element);
+        }
+      });
+      root_element.append(materials)
+    }
+  
+    //process_inputs
+    add_materials_to_xml(general_recipe, workspace_items, elementsWithSourceConnection, "ProcessInputs")
+  
+    //process outputs
+    add_materials_to_xml(general_recipe, workspace_items, elementsWithTargetConnection, "ProcessOutputs")
+  
+    //Other information
+    var other_information = xmlDocument.createElement('OtherInformation')
+      //process_outputs.appendChild()
+    general_recipe.append(other_information)
+  
+    //Process Element Parameter
+    var process_element_parameter = xmlDocument.createElement('ProzessElementParameter')
+      //process_outputs.appendChild()
+    general_recipe.append(process_element_parameter)
+  
+    //Process Intermediate
+    var prozess_intermediate = xmlDocument.createElement('ProzessIntermediate')
+      //process_outputs.appendChild()
+    general_recipe.append(prozess_intermediate)
+  
+    batchML.appendChild(general_recipe)
+  
+  
+    // Iterate over connections and create XML elements
+    var connections = jsplumb_connections;
+    connections.forEach(function (connection) {
+      console.log(connection)
+      var sourceElementId = connection.sourceId;
+      var targetElementId = connection.targetId;
+  
+      // Look up the corresponding items in the workspace_items list based on the element IDs
+      var sourceItem = workspace_items.find(item => item.id === sourceElementId);
+      var targetItem = workspace_items.find(item => item.id === targetElementId);
+  
+      if (sourceItem && targetItem) {
+        var sourceId = sourceItem.id; // Retrieve source element ID from the item
+        var targetId = targetItem.id; // Retrieve target element ID from the item
+  
+        var connectionElement = xmlDocument.createElement('Connection');
+        connectionElement.setAttribute('sourceId', sourceId);
+        connectionElement.setAttribute('targetId', targetId);
+        // You can add more attributes or data to the connectionElement as needed
+        batchML.appendChild(connectionElement);
+      }
+    });
+  
+    // Convert XML document to string
+    var serializer = new XMLSerializer();
+    var xmlString = serializer.serializeToString(xmlDocument);
+  
+    // You can now use the xmlString as needed, e.g., save it to a file
+    console.log(xmlString);
+  
+    //automatically start download
+    var filename = "Verfahrensrezept.xml";
+    var pom = document.createElement('a');
+    var bb = new Blob([xmlString], {type: 'text/plain'});
+    pom.setAttribute('href', window.URL.createObjectURL(bb));
+    pom.setAttribute('download', filename);
+    pom.dataset.downloadurl = ['text/plain', pom.download, pom.href].join(':');
+    pom.draggable = true; 
+    pom.classList.add('dragout');
+  
+    pom.click();
+  }
--- client/src/components/workspace.vue
+++ client/src/components/workspace.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref, onMounted, onUpdated, watch, nextTick } from 'vue';
 import { newInstance, ready } from "@jsplumb/browser-ui";
+import { export_batchml } from './export_xml.js';
 
 // when an element is dropped into the workspace workspace_items 
 const workspace_items = ref([]);
@@ -98,189 +99,6 @@ async function updateItemList(newItems) {
 }
 watch(workspace_items, updateItemList, { deep: true });
 
-
-//create an BatchML XML Document containing all Connections and Elements
-function export_batchml() {
-  // Create an XML document
-  var xmlDocument = document.implementation.createDocument("", "BatchInformation");
-  
-  // Specify the XML version at the top
-  const xmlDeclaration = xmlDocument.createProcessingInstruction('xml', 'version="1.0"');
-  xmlDocument.insertBefore(xmlDeclaration, xmlDocument.firstChild);
-  
-  // Get the root element
-  var batchML = xmlDocument.documentElement;
-  var general_recipe = xmlDocument.createElement('GeneralRecipe')
-  
-  //append top level elements
-  general_recipe.appendChild(xmlDocument.createElement('Description'));
-  general_recipe.appendChild(xmlDocument.createElement('ID'));
-  general_recipe.appendChild(xmlDocument.createElement('Version'));
-  
-  //create header and append
-  var header = xmlDocument.createElement('Header')
-  //modification log
-  var mod_log = xmlDocument.createElement('ModificationLog')
-  mod_log.appendChild(xmlDocument.createElement("ModifiedDate"));
-  mod_log.appendChild(xmlDocument.createElement("Author"));
-  header.appendChild(mod_log)
-  //aproval History
-  var approval_history = xmlDocument.createElement('ApprovalHistory')
-  approval_history.appendChild(xmlDocument.createElement('FinalApprovalDate'));
-  approval_history.appendChild(xmlDocument.createElement('Version'));
-  //individual approval
-  var individual_approval = xmlDocument.createElement('IndividualApproval');
-  individual_approval.appendChild(xmlDocument.createElement('ApprovedBy'));
-  individual_approval.appendChild(xmlDocument.createElement('ApprovalDate'));
-  approval_history.appendChild(individual_approval)
-  header.appendChild(approval_history)
-
-  header.appendChild(xmlDocument.createElement('EffectiveDate'));
-  header.appendChild(xmlDocument.createElement('ProductID'));
-  header.appendChild(xmlDocument.createElement('ProductName'));
-  
-  // batch size
-  var batch_size = xmlDocument.createElement('BatchSize');
-  batch_size.appendChild(xmlDocument.createElement('Nominal'));
-  batch_size.appendChild(xmlDocument.createElement('Normal'));
-  batch_size.appendChild(xmlDocument.createElement('Min'));
-  batch_size.appendChild(xmlDocument.createElement('Max'));
-  batch_size.appendChild(xmlDocument.createElement('ScaleReference'));
-  batch_size.appendChild(xmlDocument.createElement('ScaledSize'));
-  batch_size.appendChild(xmlDocument.createElement('UnitOfMeasure'));
-  header.appendChild(batch_size)
-
-  general_recipe.appendChild(header);
-
-  //Prozess Procedure
-  var process_procedure = xmlDocument.createElement('ProcessProcedure')
-    // Iterate over workspace items and create XML elements
-    workspace_items.value.forEach(function (item) {
-      if(item.type == "process"){
-        var process_element = xmlDocument.createElement('ProcessElement');
-        process_element.setAttribute('id', item.id);
-        process_element.setAttribute('name', item.name);
-        process_element.setAttribute('type', item.type);
-        // You can add more attributes or data to the itemElement as needed
-        process_procedure.appendChild(process_element);
-      }
-    });
-  general_recipe.append(process_procedure)
-
-  // Assuming you have a list of elements and connections
-  var connections = instance.getConnections();
-
-  var elementsWithSourceConnection = [];
-  var elementsWithTargetConnection = [];
-
-  // Iterate through connections and collect elements
-  for (var connectionId in connections) {
-    var connection = connections[connectionId];
-    var sourceId = connection.sourceId;
-    var targetId = connection.targetId;
-
-    // Find source and target elements
-    var sourceElement = document.getElementById(sourceId);
-    var targetElement = document.getElementById(targetId);
-
-    // Check if source element already added to list
-    if (elementsWithSourceConnection.indexOf(sourceElement) === -1) {
-      elementsWithSourceConnection.push(sourceElement);
-    }
-
-    // Check if target element already added to list
-    if (elementsWithTargetConnection.indexOf(targetElement) === -1) {
-      elementsWithTargetConnection.push(targetElement);
-    }
-  }
-
-  console.log("Elements with source connection:", elementsWithSourceConnection);
-  console.log("Elements with target connection:", elementsWithTargetConnection);
-
-  // Adds either input or output materials to xml file
-  function add_materials_to_xml(root_element, workspace_items, elements_list, material_type){
-    var materials = xmlDocument.createElement(material_type)
-    elements_list.forEach(function (item) {
-      var workspace_item = workspace_items.value.find(x => x.id === item.id);
-      if(workspace_item.type == "material"){
-        var process_element = xmlDocument.createElement('ProcessElement');
-        process_element.setAttribute('id', workspace_item.id);
-        process_element.setAttribute('name', workspace_item.name);
-        process_element.setAttribute('type', workspace_item.type);
-        // You can add more attributes or data to the itemElement as needed
-        materials.appendChild(process_element);
-      }
-    });
-    root_element.append(materials)
-  }
-
-  //process_inputs
-  add_materials_to_xml(general_recipe, workspace_items, elementsWithSourceConnection, "ProcessInputs")
-
-  //process outputs
-  add_materials_to_xml(general_recipe, workspace_items, elementsWithTargetConnection, "ProcessOutputs")
-
-  //Other information
-  var other_information = xmlDocument.createElement('OtherInformation')
-    //process_outputs.appendChild()
-  general_recipe.append(other_information)
-
-  //Process Element Parameter
-  var process_element_parameter = xmlDocument.createElement('ProzessElementParameter')
-    //process_outputs.appendChild()
-  general_recipe.append(process_element_parameter)
-
-  //Process Intermediate
-  var prozess_intermediate = xmlDocument.createElement('ProzessIntermediate')
-    //process_outputs.appendChild()
-  general_recipe.append(prozess_intermediate)
-
-  batchML.appendChild(general_recipe)
-
-
-  // Iterate over connections and create XML elements
-  var connections = instance.getConnections();
-  connections.forEach(function (connection) {
-    console.log(connection)
-    var sourceElementId = connection.sourceId;
-    var targetElementId = connection.targetId;
-
-    // Look up the corresponding items in the workspace_items list based on the element IDs
-    var sourceItem = workspace_items.value.find(item => item.id === sourceElementId);
-    var targetItem = workspace_items.value.find(item => item.id === targetElementId);
-
-    if (sourceItem && targetItem) {
-      var sourceId = sourceItem.id; // Retrieve source element ID from the item
-      var targetId = targetItem.id; // Retrieve target element ID from the item
-
-      var connectionElement = xmlDocument.createElement('Connection');
-      connectionElement.setAttribute('sourceId', sourceId);
-      connectionElement.setAttribute('targetId', targetId);
-      // You can add more attributes or data to the connectionElement as needed
-      batchML.appendChild(connectionElement);
-    }
-  });
-
-  // Convert XML document to string
-  var serializer = new XMLSerializer();
-  var xmlString = serializer.serializeToString(xmlDocument);
-
-  // You can now use the xmlString as needed, e.g., save it to a file
-  console.log(xmlString);
-
-  //automatically start download
-  var filename = "Verfahrensrezept.xml";
-  var pom = document.createElement('a');
-  var bb = new Blob([xmlString], {type: 'text/plain'});
-  pom.setAttribute('href', window.URL.createObjectURL(bb));
-  pom.setAttribute('download', filename);
-  pom.dataset.downloadurl = ['text/plain', pom.download, pom.href].join(':');
-  pom.draggable = true; 
-  pom.classList.add('dragout');
-
-  pom.click();
-}
-
 // TODO: check if this interfers with js plumb drag and drop as it may be called on every drop event
 const onDrop = (event) => {
   console.log("Drop");
@@ -318,7 +136,7 @@ const onDrop = (event) => {
 <!--Draw all workspace elements. Connections are drawn by jsplumb in the background-->
 <template>
   <div id="workspace" ref="workspace" @drop="$event => onDrop($event)" @dragenter.prevent @dragover.prevent>
-    <button @click="export_batchml" id="export_button">
+    <button @click="export_batchml(workspace_items, instance.getConnections())" id="export_button">
       <span class="toggle-icons">export</span>
     </button>
     <div :class="'workspace_element ' + item.type" v-for="item in workspace_items" :key="item.id" :ref=" skipUnwrap.jsplumbElements" :id="item.id">
