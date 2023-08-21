5977ee4b051bafe0fae1556d02cabf1a7a7e9351
Sebastian Ulrich <sp.ulrich@hotmail.de>
2023-08-21 13:26:41 +0200

    generalized xml-element creation -added function to create process elements -process procedure is now added as a process_element -this allows for easy adding of makro steps later -process elements are recursive -add_element_list function to add arrays and single objects to xml

--- client/src/components/export_xml.js
+++ client/src/components/export_xml.js
@@ -1,38 +1,118 @@
 function create_Element_with_inner_HTML(xmlDocument, name, innerHTML){
-  var element = xmlDocument.createElement(name)
-  element.innerHTML = innerHTML
-  return element
+  if (innerHTML !== undefined){
+    console.log("innerHTML defined")
+    var element = xmlDocument.createElement(name)
+    element.innerHTML = innerHTML
+    return element
+  }else{
+    console.log("innerHTML undefined, return undefined")
+    return undefined
+  }
 }
 
-function create_materials_type(xmlDocument, name){
-    var materials_type = xmlDocument.createElement(name)
-    materials_type.appendChild(create_Element_with_inner_HTML(xmlDocument, "ID", "testid"))
-    materials_type.appendChild(create_Element_with_inner_HTML(xmlDocument, "Description", "testdescription"))
-    materials_type.appendChild(create_Element_with_inner_HTML(xmlDocument, "MaterialsType", "testmaterialstype"))
-    materials_type.appendChild(create_Element_with_inner_HTML(xmlDocument, "material", "testmaterial"))
+function append_Element_if_exists(parent, children){
+  if (children === undefined){
+    return parent
+  }else{
+    console.log("append nothing to " + parent.nodeName +" as child is none")
+  }
+  // if it is list append all children
+  console.log("append child " + children.nodeName + " to " + parent.nodeName)
+  if(Array.isArray(children)){
+    // if it is list append all children
+    children.forEach(child => {
+      parent.appendChild(child)
+    })
+  }else{
+    // if it is just one object append directly
+    console.log("not an array")
+    console.log(children)
+    parent.appendChild(children)
+  }
+  return parent
 }
 
-function create_process_element_type(xmlDocument, name, id, description, type){
+function create_materials_type(xmlDocument, name, materials){
+  if (materials === undefined){
+    return undefined
+  }
+  var materials_type = xmlDocument.createElement(name)
+  materials_type = append_Element_if_exists(materials_type, create_Element_with_inner_HTML(xmlDocument, "ID", materials.ID))
+  materials_type = append_Element_if_exists(materials_type,create_Element_with_inner_HTML(xmlDocument, "Description",  "test"))
+  materials_type = append_Element_if_exists(materials_type,create_Element_with_inner_HTML(xmlDocument, "MaterialsType",  materials.MaterialsType))
+  materials_type = append_Element_if_exists(materials_type,create_Element_with_inner_HTML(xmlDocument, "material", "testmaterial"))
+  return materials_type
+}
+
+function create_directed_link(xmlDocument, name, directed_link){
+  if (directed_link === undefined){
+    return undefined
+  }
+  var directed_link_xml_obj = xmlDocument.createElement(name)
+  directed_link_xml_obj = append_Element_if_exists(directed_link_xml_obj, create_Element_with_inner_HTML(xmlDocument, "ID", directed_link.ID))
+  directed_link_xml_obj = append_Element_if_exists(directed_link_xml_obj, create_Element_with_inner_HTML(xmlDocument, "Description",  directed_link.Description))
+  directed_link_xml_obj = append_Element_if_exists(directed_link_xml_obj, create_Element_with_inner_HTML(xmlDocument, "FromID",  directed_link.FromID))
+  directed_link_xml_obj = append_Element_if_exists(directed_link_xml_obj, create_Element_with_inner_HTML(xmlDocument, "ToID", directed_link.ToID))
+  return directed_link_xml_obj
+}
+
+function create_process_element_type(xmlDocument, name, process_element){
+  if (process_element === undefined){
+    return undefined
+  }
   //only supports micro steps yet
   //therefore fields for makro steps are not yet filled
-  var process_element_type = xmlDocument.createElement("ProcessElement")
-  process_element_type.appendChild(create_Element_with_inner_HTML(xmlDocument, "ID", id))
-  process_element_type.appendChild(create_Element_with_inner_HTML(xmlDocument, "Description", name+":"+description))
-  process_element_type.appendChild(create_Element_with_inner_HTML(xmlDocument, "ProcessElementType", type))
+  var process_element_type = xmlDocument.createElement(name)
+  process_element_type = append_Element_if_exists(process_element_type, create_Element_with_inner_HTML(xmlDocument, "ID", process_element.ID))
+  process_element_type = append_Element_if_exists(process_element_type, create_Element_with_inner_HTML(xmlDocument, "Description", process_element.Description))
+  process_element_type = append_Element_if_exists(process_element_type, create_Element_with_inner_HTML(xmlDocument, "ProcessElementType", process_element.ProcessElementType))
   
-  /* IF MAKRO
-  process_element_type.appendChild(create_Element_with_inner_HTML(xmlDocument, "LifeCycleState", ""))
-  process_element_type.appendChild(create_Element_with_inner_HTML(xmlDocument, "SequenceOrder", ""))
-  process_element_type.appendChild(create_Element_with_inner_HTML(xmlDocument, "SequencePath", ""))
-  process_element_type.appendChild(create_Element_with_inner_HTML(xmlDocument, "Materials", ""))
-  process_element_type.appendChild(create_Element_with_inner_HTML(xmlDocument, "DirectedLink", ""))
-  process_element_type.appendChild(create_Element_with_inner_HTML(xmlDocument, "ProcedureChartElement", ""))
-  process_element_type.appendChild(create_Element_with_inner_HTML(xmlDocument, "ProcessElement", ""))
-  process_element_type.appendChild(create_Element_with_inner_HTML(xmlDocument, "ProcessElementParameter", ""))
-  process_element_type.appendChild(create_Element_with_inner_HTML(xmlDocument, "RessouceConstraint", ""))
-  process_element_type.appendChild(create_Element_with_inner_HTML(xmlDocument, "OtherInformation", ""))
-  */
- return process_element_type
+  // IF MAKRO
+  process_element_type = append_Element_if_exists(process_element_type, create_Element_with_inner_HTML(xmlDocument, "LifeCycleState", process_element.LifeCycleState))
+  process_element_type = append_Element_if_exists(process_element_type, create_Element_with_inner_HTML(xmlDocument, "SequenceOrder", process_element.SequenceOrder))
+  process_element_type = append_Element_if_exists(process_element_type, create_Element_with_inner_HTML(xmlDocument, "SequencePath", process_element.SequencePath))
+  process_element_type = append_Element_if_exists(process_element_type, create_materials_type(xmlDocument, "Materials", process_element.Materials))
+  process_element_type = append_Element_if_exists(process_element_type, add_list(create_directed_link, xmlDocument, "DirectedLink", process_element.DirectedLink))
+  process_element_type = append_Element_if_exists(process_element_type, create_Element_with_inner_HTML(xmlDocument, "ProcedureChartElement", process_element.ProcedureChartElement))
+  console.log(process_element.ProcessElement)
+  process_element_type = append_Element_if_exists(process_element_type, create_process_element_types(xmlDocument, "ProcessElement", process_element.ProcessElement))
+  process_element_type = append_Element_if_exists(process_element_type, create_Element_with_inner_HTML(xmlDocument, "ProcessElementParameter", process_element.ProcessElementParameter))
+  process_element_type = append_Element_if_exists(process_element_type, create_Element_with_inner_HTML(xmlDocument, "RessouceConstraint", process_element.RessouceConstraint))
+  process_element_type = append_Element_if_exists(process_element_type, create_Element_with_inner_HTML(xmlDocument, "OtherInformation", process_element.OtherInformation))
+  console.log(process_element_type)
+  return process_element_type
+}
+
+function add_list(add_element_function, xmlDocument, name, element_list){
+  if (element_list === undefined){
+    return undefined
+  }
+  var elements = []
+  if (Array.isArrayelement_list){
+    element_list.forEach(element =>{
+      elements.push(add_element_function(xmlDocument, name, element))
+    })
+  }else{
+    console.log("not an array")
+    elements.push(add_element_function(xmlDocument, name, element_list))
+  }
+ return elements
+}
+
+function create_process_element_types(xmlDocument, name, process_elements){
+  if (process_elements === undefined){
+    return undefined
+  }
+  var elements = []
+  if (Array.isArray(process_elements)){
+    process_elements.forEach(process_element =>{
+      elements.push(create_process_element_type(xmlDocument, name, process_element))
+    })
+  }else{
+    console.log("not an array")
+    elements.push(create_process_element_type(xmlDocument, name, process_elements))
+  }
+ return elements
 }
 
 
@@ -110,19 +190,31 @@ function create_formula(xmlDocument, workspace_items, jsplumb_connections){
   //ProcessInputs (set of MaterialsType)
   var process_inputs = xmlDocument.createElement('ProcessInputs')
   //HERE FOR LOOP
-  create_materials_type(xmlDocument, "Materials")
+  create_materials_type(xmlDocument, "Materials", {
+    "Name": "Materials",
+    "ID": "testid", 
+    "MaterialsType": "testMaterialstype"
+  })
   formula.appendChild(process_inputs)
 
   //ProcessOutputs (set of MaterialsType)
   var process_outputs = xmlDocument.createElement('ProcessOutputs')
   //HERE FOR LOOP
-  create_materials_type(xmlDocument, "Materials")
+  create_materials_type(xmlDocument, "Materials", {
+    "Name": "Materials",
+    "ID": "testid", 
+    "MaterialsType": "testMaterialstype"
+  })
   formula.appendChild(process_outputs)
 
   //ProcessIntermediates (set of MaterialsType)
   var process_intermediates = xmlDocument.createElement('ProcessIntermediates')
   //HERE FOR LOOP
-  create_materials_type(xmlDocument, "Materials")
+  create_materials_type(xmlDocument, "Materials", {
+    "Name": "Materials",
+    "ID": "testid", 
+    "MaterialsType": "testMaterialstype"
+  })
   formula.appendChild(process_intermediates)
 
   //ProcessElementParameter
@@ -132,48 +224,65 @@ function create_formula(xmlDocument, workspace_items, jsplumb_connections){
   return formula
 }
 
+
 function create_process_procedure(xmlDocument, workspace_items, jsplumb_connections){
   //Prozess Procedure
-  var process_procedure = xmlDocument.createElement('ProcessProcedure')
+  var process_procedure_obj = {
+    "Name": "ProcessProcedure",
+    "ID": "", 
+    "ProcessElementType": "Process Procedure"
+  }
 
   // Iterate over workspace items to create XML elements
+  var process_elements = []
   workspace_items.forEach(function (item) {
     if(item.type == "process"){
-      var process_element = create_process_element_type(
-                            xmlDocument,
-                            item.name, 
-                            item.id, 
-                            "",
-                            "Process Action"
-                          )
-      process_procedure.appendChild(process_element);
+      process_elements.push(
+        {
+          "Name": "ProcessElement",
+          "ID": item.id, 
+          "ProcessElementType": "Process Action"
+        }
+      )
     }; 
   });
+
+  process_procedure_obj.ProcessElement = process_elements
 
   // Iterate over workspace items to create Material XML elements
+  var materials = [] 
   workspace_items.forEach(function (item) {
     if(item.type == "material"){
-      var materials = xmlDocument.createElement('materials');
-      materials.appendChild(create_Element_with_inner_HTML(xmlDocument, "ID", item.id))
-      materials.appendChild(create_Element_with_inner_HTML(xmlDocument, "Description", ""))
-      materials.appendChild(create_Element_with_inner_HTML(xmlDocument, "MaterialsType", "testMaterialstype"))
-
-      // You can add more attributes or data to the itemElement as needed
-      process_procedure.appendChild(materials);
+      
+      materials.push(
+        {
+          "Name": "Materials",
+          "ID": item.id, 
+          "MaterialsType": "testMaterialstype"
+        }
+      )
     }; 
   });
+  process_procedure_obj.Materials = materials
 
   // Iterate over connections and create Directed Links
+  var directed_links = []
   for (var connectionId in jsplumb_connections) {
-    var connection = jsplumb_connections[connectionId];
-      var directed_link = xmlDocument.createElement('DirectedLink')
-      directed_link.appendChild(create_Element_with_inner_HTML(xmlDocument, "ID", connectionId))
-      directed_link.appendChild(create_Element_with_inner_HTML(xmlDocument, "Description", ""))
-      directed_link.appendChild(create_Element_with_inner_HTML(xmlDocument, "FromID", connection.sourceId))
-      directed_link.appendChild(create_Element_with_inner_HTML(xmlDocument, "TOID", connection.targetId))
-      process_procedure.appendChild(directed_link)
+    var connection = jsplumb_connections[connectionId]
+    directed_links.push(
+      {
+        "Name":"DirectedLink",
+        "ID": connectionId,
+        "Description": "",
+        "FromID": connection.sourceId,
+        "ToID": connection.targetId
+      }
+    )
   }
-  return process_procedure
+  
+  process_procedure_obj.DirectedLink = directed_links
+
+  return create_process_element_type(xmlDocument, "ProcessProcedure", process_procedure_obj)
 }
 
 
@@ -205,6 +314,7 @@ export function export_batchml(workspace_items, jsplumb_connections) {
 
     //Process Procedure
     var process_procedure = create_process_procedure(xmlDocument, workspace_items, jsplumb_connections)
+    console.log(process_procedure)
     general_recipe.appendChild(process_procedure)
   
     //Other information
