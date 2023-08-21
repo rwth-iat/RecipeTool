acbd7c281077137b0d25c936f05ae59fc62afde6
Sebastian Ulrich <sp.ulrich@hotmail.de>
2023-08-21 16:43:39 +0200

    added coverage packages to client and server

--- client/vite.config.js
+++ client/vite.config.js
@@ -12,6 +12,10 @@ export default defineConfig({
   },
   test: {
     globals   : true,
+    environment: "jsdom",
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+    },
   },  
   plugins: [
     vue(),
