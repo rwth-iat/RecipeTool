b6c4f96de951ba324ba87a6f2c2258e392bcd26f
Sebastian Ulrich <sp.ulrich@hotmail.de>
2023-12-12 19:43:57 +0100

    added validate AASX function

--- server/AasAPI.py
+++ server/AasAPI.py
@@ -1,6 +1,13 @@
 from flask import Blueprint, request, make_response, flash
 import xml.etree.ElementTree as ET
-
+from basyx.aas.compliance_tool import compliance_check_aasx
+from basyx.aas.compliance_tool import compliance_check_xml as compliance_tool_xml, \
+    compliance_check_json as compliance_tool_json, compliance_check_aasx as compliance_tool_aasx
+from basyx.aas.adapter.json import write_aas_json_file
+from basyx.aas.examples.data import create_example, create_example_aas_binding, TEST_PDF_FILE
+from basyx.aas.compliance_tool.state_manager import ComplianceToolStateManager, Status
+import tempfile
+import os
 
 def get_aasx_id(file_content):
   root = ET.fromstring(file_content)
@@ -56,5 +63,46 @@ def get_aasx_capabilities():
     file_content = file.read()
     capabilities = get_all_aasx_capabilities(file_content)
     return capabilities
+  
+@aas_api.route('/AASX/validate', methods=['POST'])
+def validate_aasx():
+    """Endpoint to validate a AASX.
+    ---
+    tags:
+      - AAS
+    parameters:
+      - name: file
+        in: formData
+        type: file
+        required: true
+    responses:
+      200:
+        description: Boolean showing if AASX is valid or not.
+        examples:
+            rgb: True
+    """
+    if 'file' not in request.files:
+      print("no file given")
+      flash('No file part')
+      return make_response(request.url, 400)
+    file = request.files['file']
+    file_content = file.read()
+    stateManager = ComplianceToolStateManager()
+    
+    with tempfile.NamedTemporaryFile(mode='wb+', delete=False) as temp_file:
+        temp_file.write(file_content)
+        temp_file_path = temp_file.name
+    try:
+        compliance_tool_aasx.check_schema(temp_file_path, stateManager) 
+    except Exception as e:
+      # Handle the exception here
+      print(f"An error occurred: {e}")
+      # You can also log the error or take any other appropriate action
+      return make_response(str(e), 400)
+    finally:
+        # Clean up: delete the temporary file
+        os.remove(temp_file_path) 
+    return make_response("True", 200)
+    
 
  
